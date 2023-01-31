c7aebe4e9e61330c95c4588e7fd2291ce83995f5
YaswanthVarma754 <konduru.varma@tringapps.com>
2023-01-31 09:45:05 +0530

    made all capitals for constants

--- src/main/java/org/example/Shape.java
+++ src/main/java/org/example/Shape.java
@@ -66,52 +66,57 @@ class Main{
 
     public static void main(String[] args) {
         Logger logger = Logger.getLogger("com.api.jar");
-        String check;
-        String type;
-        double triangleL1;
-        double triangleL2;
-        double triangleL3;
-        double triangleHeight;
-        double triangleBase;
-        double rectLength;
-        double rectWidth;
-        double radius;
-        Shape s;
-        Scanner sc = new Scanner(System.in);
-        logger.info("Enter Shape type : ");
-        type = sc.next();
-        if (type.equals(CIRCLE)) {
-            logger.info("Enter radius of the CIRCLE : ");
-            radius = sc.nextDouble();
+        try {
+            String check;
+            String type;
+            double triangleL1;
+            double triangleL2;
+            double triangleL3;
+            double triangleHeight;
+            double triangleBase;
+            double rectLength;
+            double rectWidth;
+            double radius;
+            Shape s;
+            Scanner sc = new Scanner(System.in);
+            logger.info("Enter Shape type : ");
+            type = sc.next();
+            if (type.equals(CIRCLE)) {
+                logger.info("Enter radius of the CIRCLE : ");
+                radius = sc.nextDouble();
 
-            s = new Shape(type, radius);
-        } else if (type.equals(RECTANGLE)) {
-            logger.info("Enter length of the RECTANGLE : ");
-            rectLength = sc.nextDouble();
-            logger.info("Enter width of the RECTANGLE : ");
-            rectWidth = sc.nextDouble();
+                s = new Shape(type, radius);
+            } else if (type.equals(RECTANGLE)) {
+                logger.info("Enter length of the RECTANGLE : ");
+                rectLength = sc.nextDouble();
+                logger.info("Enter width of the RECTANGLE : ");
+                rectWidth = sc.nextDouble();
 
-            s = new Shape(type, rectLength, rectWidth);
-        } else {
-            logger.info("Enter 1st side of the triangle : ");
-            triangleL1 = sc.nextDouble();
-            logger.info("Enter 2nd side of the triangle : ");
-            triangleL2 = sc.nextDouble();
-            logger.info("Enter 3rd side of the triangle : ");
-            triangleL3 = sc.nextDouble();
-            logger.info("Enter height of the triangle : ");
-            triangleHeight = sc.nextDouble();
-            logger.info("Enter base of the triangle : ");
-            triangleBase = sc.nextDouble();
+                s = new Shape(type, rectLength, rectWidth);
+            } else {
+                logger.info("Enter 1st side of the triangle : ");
+                triangleL1 = sc.nextDouble();
+                logger.info("Enter 2nd side of the triangle : ");
+                triangleL2 = sc.nextDouble();
+                logger.info("Enter 3rd side of the triangle : ");
+                triangleL3 = sc.nextDouble();
+                logger.info("Enter height of the triangle : ");
+                triangleHeight = sc.nextDouble();
+                logger.info("Enter base of the triangle : ");
+                triangleBase = sc.nextDouble();
 
-            s = new Shape(type, triangleL1, triangleL2, triangleL3, triangleHeight,
-                    triangleBase);
+                s = new Shape(type, triangleL1, triangleL2, triangleL3, triangleHeight,
+                        triangleBase);
+            }
+            check = "Perimeter for the Shape : " + s.perimeter();
+            logger.info(check);
+            check = "Area of the shape : " + s.area();
+            logger.info(check);
+            sc.close();
         }
-        check = "Perimeter for the Shape : " + s.perimeter();
-        logger.info(check);
-        check = "Area of the shape : " + s.area();
-        logger.info(check);
-        sc.close();
+        catch (Exception e) {
+            logger.info("Something went wrong while giving input");
+        }
 
     }
 }
